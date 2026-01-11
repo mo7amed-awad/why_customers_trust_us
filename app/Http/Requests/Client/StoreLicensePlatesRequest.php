d8ecc36d23d1346b34b2b7455581db91ae493461
mo7amed-awad <ma7amedawad@gmail.com>
2026-01-11 12:01:06 +0200

    edit

--- app/Http/Requests/Client/StoreLicensePlatesRequest.php
+++ app/Http/Requests/Client/StoreLicensePlatesRequest.php
@@ -3,21 +3,40 @@
 namespace App\Http\Requests\Client;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreLicensePlatesRequest extends FormRequest
 {
-
     public function authorize(): bool
     {
         return true;
     }
 
-
     public function rules(): array
     {
         return [
-            'digit_count' => 'required|in:1,2,3,4,5,6',
-            'usage_type' => 'required|in:private,commercial,government,diplomatic,antique',
+            'digit_count' => ['required', Rule::in([1, 2, 3, 4, 5, 6])],
+
+            'usage_type' => [
+                'required',
+                Rule::in([
+                    'public_shared',
+                    'public_transport',
+                    'taxi',
+                    'motorcycle',
+                    'tourist_bus',
+                    'contracting',
+                    'private',
+                    'private_passengers',
+                    'shared_private',
+                    'commercial_transport',
+                    'semi_trailer',
+                    'antique',
+                    'rental',
+                    'on_demand_taxi',
+                    'public_passengers',
+                ]),
+            ],
         ];
     }
 }
--- resources/views/Client/Ads/partials/license-plates-fields.blade.php
+++ resources/views/Client/Ads/partials/license-plates-fields.blade.php
@@ -28,63 +28,51 @@
 <div class="col-md-11 border-bottom py-lg-4 py-2">
     <div class="row justify-content-between gap-lg-0 gap-3">
         <div class="col-lg-2">
-            <label class="form-label fw-bold primary-color">{{ __('front.usage_type') }}</label>
+            <label for="usage_type" class="form-label fw-bold primary-color">
+                {{ __('front.usage_type') }}
+            </label>
         </div>
+
         <div class="col-lg-9">
-            <div class="d-flex flex-wrap gap-2">
+            <div class="input-group bg-transparent border rounded-3 flex-grow-1">
+                <select name="usage_type"
+                        id="usage_type"
+                        class="form-select bg-transparent border-0"
+                        required>
 
-                <div class="form-check px-0">
-                    <input type="radio"
-                           class="form-check-input"
-                           name="usage_type"
-                           id="usage_private"
-                           value="private"
-                           required
-                            @checked(old('usage_type', 'private') == 'private')>
-                    <label class="form-check-label" for="usage_private">{{ __('front.private') }}</label>
-                </div>
+                    <option value="">
+                        -- {{ __('front.select_usage_type') }} --
+                    </option>
 
-                <div class="form-check">
-                    <input type="radio"
-                           class="form-check-input"
-                           name="usage_type"
-                           id="usage_commercial"
-                           value="commercial"
-                            @checked(old('usage_type') == 'commercial')>
-                    <label class="form-check-label" for="usage_commercial">{{ __('front.commercial') }}</label>
-                </div>
+                    @php
+                        $usageTypes = [
+                            'public_shared',
+                            'public_transport',
+                            'taxi',
+                            'motorcycle',
+                            'tourist_bus',
+                            'contracting',
+                            'private_plate',
+                            'private_passengers',
+                            'shared_private',
+                            'commercial_transport',
+                            'semi_trailer',
+                            'antique',
+                            'rental',
+                            'on_demand_taxi',
+                            'public_passengers',
+                        ];
+                    @endphp
 
-                <div class="form-check">
-                    <input type="radio"
-                           class="form-check-input"
-                           name="usage_type"
-                           id="usage_government"
-                           value="government"
-                            @checked(old('usage_type') == 'government')>
-                    <label class="form-check-label" for="usage_government">{{ __('front.government') }}</label>
-                </div>
+                    @foreach ($usageTypes as $type)
+                        <option value="{{ $type }}" @selected(old('usage_type') == $type)>
+                            {{ __('front.'.$type) }}
+                        </option>
+                    @endforeach
 
-                <div class="form-check">
-                    <input type="radio"
-                           class="form-check-input"
-                           name="usage_type"
-                           id="usage_diplomatic"
-                           value="diplomatic"
-                            @checked(old('usage_type') == 'diplomatic')>
-                    <label class="form-check-label" for="usage_diplomatic">{{ __('front.diplomatic') }}</label>
-                </div>
-
-                <div class="form-check">
-                    <input type="radio"
-                           class="form-check-input"
-                           name="usage_type"
-                           id="usage_antique"
-                           value="antique"
-                            @checked(old('usage_type') == 'antique')>
-                    <label class="form-check-label" for="usage_antique">{{ __('front.antique') }}</label>
-                </div>
-
+                </select>
             </div>
+
             @error('usage_type')
             <small class="text-danger d-block mt-1">{{ $message }}</small>
             @enderror
